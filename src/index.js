cc48258ec63064a8f447183ff8f1df15166cf5c2
Gerardo Mayboca <gerardo@risingcloud.com>
2023-11-01 18:06:18 -0700

    Started project with core libraries

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import '@fontsource/inter';
 import reportWebVitals from './reportWebVitals';
+import DashboardPage from './pages/dashboard';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DashboardPage />
   </React.StrictMode>
 );
 
---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <main>
+        <h1>Hello World</h1>
+    </main>
+  )
+}
+
+export default DashboardPage
